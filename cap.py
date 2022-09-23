0a4321477dbbcc3a46261b88c528de5987d0c113
Alaa <alaa_elyassir@hotmail.com>
2022-09-23 08:51:24 +0200

    training

--- cap.py
+++ cap.py
@@ -1,24 +1,28 @@
 #-*- coding: utf8-*-
 '''module cap'''
 
-from __future__ import print_function
-from __future__ import unicode_literals
 
-class Paragraphe(unicode):
+class Paragraphe(str):
     '''add cap and iscap methods to str'''
     def cap(self):
         ''' ceci est une chaine. une simple chaine ->  Ceci est une chaine. Une simple chaine'''
-        if '.' not in self:
-            return self.lstrip().capitalize().rjust(len(self))
-        else:
-            return '.'.join([Paragraphe(e).cap() for e in self.split('.')])
+        phrases = self.split('.') 
+        for indice, phrase in enumerate(phrases):
+            phrase_ = phrase.lstrip()
+            phrase_ = phrase_.capitalize()
+            phrase_ = phrase_.rjust(len(phrase))
+            phrases[indice] = phrase_
+        return Paragraphe('.'.join(phrases))
 
     def iscap(self):
         '''paragraphe == paragraphe.cap() -> True'''
         return self == self.cap()
 
 
-if __name__ == '__main__':
+def main():
     paragraphe1 = Paragraphe('ceci est une chaine. une simple chaine de caractères')
     print(paragraphe1.cap())
     print(paragraphe1.iscap())
+
+if __name__ == '__main__':
+    main()
